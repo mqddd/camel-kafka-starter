4ec24f41c1e21e80a7402f16b3c1609b565debb0
mqddd <meghdadd78@gmail.com>
2021-04-08 19:09:41 +0430

    springboot project created based on java 11 and springboot 2.4.4

---
+++ src/main/java/com/interview/camelkafkastarter/CamelKafkaStarterApplication.java
@@ -0,0 +1,14 @@
+package com.interview.camelkafkastarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelKafkaStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelKafkaStarterApplication.class, args);
+//        System.out.println("fhf");
+    }
+
+}
---
+++ src/test/java/com/interview/camelkafkastarter/CamelKafkaStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.camelkafkastarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelKafkaStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
