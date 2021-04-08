fc4ba303a1d3502e309ee1511fd075e30b0ea5fb
mqddd <meghdadd78@gmail.com>
2021-04-09 01:34:54 +0430

    simple consumer app created

--- src/main/java/com/interview/camelkafkastarter/CamelKafkaStarterApplication.java
+++ src/main/java/com/interview/camelkafkastarter/CamelKafkaStarterApplication.java
@@ -1,7 +1,15 @@
 package com.interview.camelkafkastarter;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
+
+
 
 @SpringBootApplication
 public class CamelKafkaStarterApplication {
@@ -11,4 +19,18 @@ public class CamelKafkaStarterApplication {
 //        System.out.println("fhf");
     }
 
+    @Bean
+    public NewTopic topic() {
+        System.out.println("topic method");
+        return TopicBuilder.name("first-topic")
+                .partitions(10)
+                .replicas(1)
+                .build();
+    }
+
+    @KafkaListener(id = "myId", topics = "first-topic")
+    public void listen(String in) {
+        System.out.println(in);
+    }
+    
 }
