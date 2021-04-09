7da962712fc9ed41399d0ce7cabaae4bdf4f0931
mqddd <meghdadd78@gmail.com>
2021-04-09 18:12:15 +0430

    consuming messages from kafka topic using camel route

--- src/main/java/com/interview/camelkafkastarter/service/CamelKafkaRoute.java
+++ src/main/java/com/interview/camelkafkastarter/service/CamelKafkaRoute.java
@@ -8,7 +8,8 @@ public class CamelKafkaRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("timer:logMessageTimer?period=5s")
+        from("kafka:first-topic?brokers=localhost:9092&groupId=myId&autoOffsetReset=earliest")
+                .process(exchange -> System.out.println(exchange.getIn().getBody()))
                 .log("event triggered!");
 
     }
