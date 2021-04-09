e1c12c25cf9ddd8c36e5c8ef1e592a0752b2f2a4
mqddd <meghdadd78@gmail.com>
2021-04-09 12:25:57 +0430

    kafka configurations added

---
+++ src/main/java/com/interview/camelkafkastarter/config/KafkaConfig.java
@@ -0,0 +1,35 @@
+package com.interview.camelkafkastarter.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public NewTopic topic() {
+        System.out.println("topic method");
+        return TopicBuilder.name("first-topic")
+                .partitions(10)
+                .replicas(1)
+                .build();
+    }
+
+    @KafkaListener(id = "myId", topics = "first-topic")
+    public void listen(String in) {
+        System.out.println("in: " + in);
+    }
+
+    @Bean
+    public ApplicationRunner runner(KafkaTemplate<String, String> template) {
+        return args -> {
+            template.send("first-topic", "test");
+        };
+    }
+
+}
