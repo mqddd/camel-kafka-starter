f43d136f7547feaecb7d0108e109bb7df6cc7cc8
mqddd <meghdadd78@gmail.com>
2021-04-09 12:29:26 +0430

    producer and consumer services added

---
+++ src/main/java/com/interview/camelkafkastarter/service/Producer.java
@@ -0,0 +1,19 @@
+package com.interview.camelkafkastarter.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Producer {
+
+    private static final String TOPIC = "first-topic";
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message){
+        System.out.println("Producing message : " + message);
+        this.kafkaTemplate.send(TOPIC, message);
+    }
+}
