4631edf0cd3919641d01d5360ae4623e4330994b
mqddd <meghdadd78@gmail.com>
2021-04-10 23:25:34 +0430

    scheduling tasks enabled

--- src/main/java/com/interview/camelkafkastarter/CamelKafkaStarterApplication.java
+++ src/main/java/com/interview/camelkafkastarter/CamelKafkaStarterApplication.java
@@ -2,8 +2,9 @@ package com.interview.camelkafkastarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@EnableScheduling
 @SpringBootApplication
 public class CamelKafkaStarterApplication {
 
