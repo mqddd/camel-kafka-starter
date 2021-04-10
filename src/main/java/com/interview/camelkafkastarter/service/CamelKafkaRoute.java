8be67d7f5778698a2e3bc44b24e7f67a6d151f66
mqddd <meghdadd78@gmail.com>
2021-04-10 23:23:17 +0430

    cosuming messages from kafka topic using camel route

--- src/main/java/com/interview/camelkafkastarter/service/CamelKafkaRoute.java
+++ src/main/java/com/interview/camelkafkastarter/service/CamelKafkaRoute.java
@@ -3,13 +3,15 @@ package com.interview.camelkafkastarter.service;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class CamelKafkaRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        final StringBuilder s = new StringBuilder("2");
         from("kafka:first-topic?brokers=localhost:9092&groupId=myId&autoOffsetReset=earliest")
-                .process(exchange -> System.out.println(exchange.getIn().getBody()))
-                .log("event triggered!");
+                .process(exchange -> System.out.println("hi : " + s.append(exchange.getIn().getBody().toString())))
+                .log(s.toString());
     }
 }
