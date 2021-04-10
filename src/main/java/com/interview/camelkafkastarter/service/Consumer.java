aa7d4e0b3da5e0cffb93d2c00ea26e385d144746
mqddd <meghdadd78@gmail.com>
2021-04-10 23:24:08 +0430

    prior kafka consume method commented

--- src/main/java/com/interview/camelkafkastarter/service/Consumer.java
+++ src/main/java/com/interview/camelkafkastarter/service/Consumer.java
@@ -1,14 +1,13 @@
 package com.interview.camelkafkastarter.service;
 
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Consumer {
 
-    @KafkaListener(topics = "first-topic", id = "myId")
-    public void consume(String message) {
-        System.out.println("Consuming message : " + message);
-    }
+//    @KafkaListener(topics = "first-topic", id = "myId")
+//    public void consume(String message) {
+//        System.out.println("Consuming message : " + message);
+//    }
 
 }
