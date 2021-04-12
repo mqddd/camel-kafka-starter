373bc220a611ecedf0ad6b097dce406ceb7fb25f
mqddd <meghdadd78@gmail.com>
2021-04-12 14:14:08 +0430

    redundant comments removed

--- src/main/java/com/interview/camelkafkastarter/service/Consumer.java
+++ src/main/java/com/interview/camelkafkastarter/service/Consumer.java
@@ -5,9 +5,4 @@ import org.springframework.stereotype.Service;
 @Service
 public class Consumer {
 
-//    @KafkaListener(topics = "first-topic", id = "myId")
-//    public void consume(String message) {
-//        System.out.println("Consuming message : " + message);
-//    }
-
 }
