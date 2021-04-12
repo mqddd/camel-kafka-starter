6a219ef3561743cfb6c573d2611c1ac40e3eaba8
mqddd <meghdadd78@gmail.com>
2021-04-12 22:57:31 +0430

    producing to kafka topic using camel ProducerTemplate

--- src/main/java/com/interview/camelkafkastarter/service/Producer.java
+++ src/main/java/com/interview/camelkafkastarter/service/Producer.java
@@ -1,7 +1,7 @@
 package com.interview.camelkafkastarter.service;
 
+import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -12,18 +12,18 @@ public class Producer {
 
     private static final String TOPIC = "first-topic";
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private ProducerTemplate producerTemplate;
 
     @Autowired
-    public Producer(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
+    public Producer(ProducerTemplate producerTemplate) {
+        this.producerTemplate = producerTemplate;
     }
 
     @Scheduled(fixedRate = 10000)
     public void sendMessage(){
         String message = String.valueOf(createRandomNumber());
         System.out.println("producing : " + message);
-        this.kafkaTemplate.send(TOPIC, message);
+        producerTemplate.sendBody("direct:kafka-producer", message);
     }
 
     private int createRandomNumber(){
