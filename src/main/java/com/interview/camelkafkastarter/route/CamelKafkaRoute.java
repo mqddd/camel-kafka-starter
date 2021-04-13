1a6ab5d7b556003992af0a5b9849c91147fe2d08
mqddd <meghdadd78@gmail.com>
2021-04-13 18:22:31 +0430

    using scheduler endpoint to producing to kafka topic with a fix rate

--- src/main/java/com/interview/camelkafkastarter/route/CamelKafkaRoute.java
+++ src/main/java/com/interview/camelkafkastarter/route/CamelKafkaRoute.java
@@ -1,6 +1,7 @@
 package com.interview.camelkafkastarter.route;
 
 import com.interview.camelkafkastarter.logic.MessageStrategy;
+import com.interview.camelkafkastarter.service.Producer;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
@@ -17,24 +18,20 @@ public class CamelKafkaRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
+        // producer route
+        from("scheduler:pro?delay=10000")
+                .bean(Producer.class, "sendMessage");
+
         // consumer routes
         from(this.KAFKA_URI).routeId(KAFKA_ROUTE_ID)
                 .aggregate(new MessageStrategy())
                 .constant(true)
                 .completionInterval(60000)
-                .to("direct:somewhere");
-
-        from("direct:somewhere")
                 .process(new Processor() {
                     @Override
                     public void process(Exchange exchange) throws Exception {
-                        System.out.println("consuming sum : " + exchange.getIn().getBody(Integer.class));
+                        System.out.println("consuming : " + exchange.getIn().getBody(Integer.class));
                     }
-                })
-                .to("mock:result");
-
-        // producer route
-        from("direct:kafka-producer")
-                .to(this.KAFKA_URI);
+                });
     }
 }
