5290b4b3bf53d171c392e42f63a3155b14cc6670
mqddd <meghdadd78@gmail.com>
2021-04-14 11:50:23 +0430

    EnableScheduling annotation removed

--- src/main/java/com/interview/camelkafkastarter/CamelKafkaStarterApplication.java
+++ src/main/java/com/interview/camelkafkastarter/CamelKafkaStarterApplication.java
@@ -2,9 +2,7 @@ package com.interview.camelkafkastarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class CamelKafkaStarterApplication {
 
