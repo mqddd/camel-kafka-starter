ed04600a1c8f1eaa5e7b571cfcc090e5b8d8674c
mqddd <meghdadd78@gmail.com>
2021-04-21 08:34:17 +0430

    migrated from junit4 to junit5

--- src/test/java/com/interview/camelkafkastarter/route/CamelKafkaRouteTest.java
+++ src/test/java/com/interview/camelkafkastarter/route/CamelKafkaRouteTest.java
@@ -4,8 +4,9 @@ import com.interview.camelkafkastarter.CamelKafkaStarterApplication;
 import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.ProducerTemplate;
-import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.builder.AdviceWith;
 import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
 import org.apache.camel.test.spring.junit5.CamelSpringTestSupport;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,7 @@ import java.util.concurrent.TimeUnit;
 
 
 @ContextConfiguration
+@CamelSpringBootTest
 @SpringBootTest(classes = CamelKafkaStarterApplication.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class CamelKafkaRouteTest extends CamelSpringTestSupport {
@@ -34,27 +36,27 @@ class CamelKafkaRouteTest extends CamelSpringTestSupport {
     @Test
     public void test() throws Exception {
 
-        AdviceWithRouteBuilder.adviceWith(context, "kafka-route", routeBuilder -> {
-            routeBuilder.replaceFromWith("direct:kafka-from");
+        AdviceWith.adviceWith(context, "kafka-route", a -> {
+            a.replaceFromWith("mock:fake-kafka");
         });
 
         result.expectedMessageCount(1);
         result.expectedBodiesReceived(6);
 
-        template.sendBody("direct:kafka-from", 1);
+        template.sendBody("mock:fake-kafka", 1);
 //        Thread.sleep(10000);
-        template.sendBody("direct:kafka-from", 1);
+        template.sendBody("mock:fake-kafka", 6);
 //        Thread.sleep(10000);
-        template.sendBody("direct:kafka-from", 1);
+        template.sendBody("mock:fake-kafka", 5);
 //        Thread.sleep(10000);
-        template.sendBody("direct:kafka-from", 1);
+        template.sendBody("mock:fake-kafka", 4);
 //        Thread.sleep(10000);
-        template.sendBody("direct:kafka-from", 1);
+        template.sendBody("mock:fake-kafka", 3);
 //        Thread.sleep(10000);
-        template.sendBody("direct:kafka-from", 1);
+        template.sendBody("mock:fake-kafka", 2);
 //        Thread.sleep(10000);
 
-        MockEndpoint.assertIsSatisfied(150, TimeUnit.SECONDS, result);
+        MockEndpoint.assertIsSatisfied(result);
     }
 
     @Override
