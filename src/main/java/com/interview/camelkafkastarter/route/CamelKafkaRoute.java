af11a6fd16a00f0b98b1e6f984a0bd1f80ee5524
mqddd <meghdadd78@gmail.com>
2021-04-25 10:44:49 +0430

    code cleaned and id added to kafka endpoint in producer route

--- src/main/java/com/interview/camelkafkastarter/route/CamelKafkaRoute.java
+++ src/main/java/com/interview/camelkafkastarter/route/CamelKafkaRoute.java
@@ -7,27 +7,28 @@ import org.springframework.stereotype.Component;
 import java.util.Random;
 
 
-@Component
+@Component(value = "n")
 public class CamelKafkaRoute extends RouteBuilder {
 
     private final String KAFKA_URI = "kafka:first-topic?brokers=localhost:9092&groupId=myId&autoOffsetReset=earliest";
 
-    public static final String KAFKA_ROUTE_ID = "kafka-route";
+    public static final String PRODUCER_ROUTE = "producer-route";
+    public static final String CONSUMER_ROUTE = "consumer-route";
 
     @Override
     public void configure() throws Exception {
 
         // producer route
-        from("scheduler:pro?delay=10000")
+        from("scheduler:pro?delay=10000").routeId(PRODUCER_ROUTE)
                 .process(exchange -> exchange.getIn().setBody(createRandomNumber()))
-                .to(KAFKA_URI);
+                    .to(KAFKA_URI).id("producer-to-kafka");
 
         // consumer routes
-        from(this.KAFKA_URI).routeId(KAFKA_ROUTE_ID)
+        from(this.KAFKA_URI).routeId(CONSUMER_ROUTE)
                 .aggregate(new MessageStrategy())
-                .constant(true)
-                .completionInterval(60000)
-                .process(exchange -> System.out.println("consuming : " + exchange.getIn().getBody(Integer.class)));
+                    .constant(true)
+                    .completionInterval(60000)
+                    .process(exchange -> System.out.println("consuming : " + exchange.getIn().getBody(Integer.class)));
     }
 
     private int createRandomNumber() {
